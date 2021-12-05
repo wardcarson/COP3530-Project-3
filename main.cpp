c07aaa4f5e3c46e1365ee7d3e4abc333b62f2be4
AnikaAChowdhury <68291596+AnikaAChowdhury@users.noreply.github.com>
2021-12-04 23:12:08 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -178,7 +178,7 @@ void Graph::readCSVAddtoGraph(string nameOfFile, map<string, City> &mapOfallCiti
             City newCity(covidCaseNo,cityName);
             std::random_device rd; // obtain a random number from hardware
             std::mt19937 gen(rd()); // seed the generator
-            std::uniform_int_distribution<> distr(0, allCities.size()-10); // define the range
+            std::uniform_int_distribution<> distr(0, allCities.size()-1); // define the range
             std::mt19937 gen2(rd()); // seed the generator
             std::uniform_int_distribution<> distr2(0, 10000); // define the range
             //auto temp = mapOfallCities[names.at(index)];
